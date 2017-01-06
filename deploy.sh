35bdbc130498618974ae3e7594b28957ff22ce2b
Stefan Scherer <scherer_stefan@icloud.com>
2017-01-06 23:13:37 +0100

    Fix image name for Linux
    Signed-off-by: Stefan Scherer <scherer_stefan@icloud.com>

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ set -e
 docker tag winspector stefanscherer/winspector:linux-$TRAVIS_TAG
 docker tag winspector stefanscherer/winspector:linux
 
-docker push stefanscherer/winspector-linux:linux-$TRAVIS_TAG
-docker push stefanscherer/winspector-linux:linux
+docker push stefanscherer/winspector:linux-$TRAVIS_TAG
+docker push stefanscherer/winspector:linux
