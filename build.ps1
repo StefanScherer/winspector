4a0f63bf89f777eca6f69d9272482ed51def5605
Stefan Scherer <scherer_stefan@icloud.com>
2017-02-07 10:58:11 +0100

    Build Windows Docker image in AppVeyor

--- build.ps1
+++ build.ps1
@@ -6,13 +6,6 @@ Write-Host Updating base images
 docker pull microsoft/windowsservercore
 docker pull microsoft/nanoserver
 
-Write-Host Removing old images
-$ErrorActionPreference = 'SilentlyContinue';
-docker rmi $(docker images --no-trunc --format '{{.Repository}}:{{.Tag}}' | sls -notmatch -pattern '(REPOSITORY|microsoft\/(windowsservercore|nanoserver))')
-$ErrorActionPreference = 'Stop';
-Write-Host Prune system
-docker system prune -f
-
 docker build -t winspector -f Dockerfile.windows .
 
 docker images
