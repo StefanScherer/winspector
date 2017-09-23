de22a70272c8ee9d3709045e96a712989a606fbd
Stefan Scherer <scherer_stefan@icloud.com>
2017-09-23 06:51:44 +0200

    List images, do not update images

--- build.ps1
+++ build.ps1
@@ -2,9 +2,12 @@ $ErrorActionPreference = 'Stop';
 $files = ""
 Write-Host Starting build
 
-Write-Host Updating base images
-docker pull microsoft/windowsservercore
-docker pull microsoft/nanoserver
+Write-Host Preinstalled images
+docker images
+
+# Write-Host Updating base images
+# docker pull microsoft/windowsservercore
+# docker pull microsoft/nanoserver
 
 docker build -t winspector -f Dockerfile.windows .
 
