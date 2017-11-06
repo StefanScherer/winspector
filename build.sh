05f6ff0d94e19b3859a0ed950f8c41b654007c8a
Stefan Scherer <scherer_stefan@icloud.com>
2017-11-06 22:33:07 +0100

    Update Docker for multi-stage

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 set -e
 
+docker version
+uname -a
+echo "Updating Docker engine to have multi-stage builds"
+sudo service docker stop
+curl -fsSL https://get.docker.com/ | sudo sh
+docker version
+
 docker build -t winspector -f Dockerfile .
