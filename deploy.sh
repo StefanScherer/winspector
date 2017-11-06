8d432b63c3985b5f2c85c657cde802c15d6babaf
Stefan Scherer <scherer_stefan@icloud.com>
2017-11-06 23:23:00 +0100

    Fix image name

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 set -e
 
-docker tag winspector stefanscherer/winspector:linux-$TRAVIS_TAG
+image="stefanscherer/winspector"
 
-docker push stefanscherer/winspector:linux-$TRAVIS_TAG
+docker tag winspector $image:linux-$TRAVIS_TAG
+docker push $image:linux-$TRAVIS_TAG
 
 set +e
-echo "Waiting for Windows image stefanscherer/winspector:windows-$TRAVIS_TAG-2016"
-until docker run --rm winspector stefanscherer/winspector:windows-$TRAVIS_TAG-2016
+echo "Waiting for Windows image $image:windows-$TRAVIS_TAG-2016"
+until docker run --rm winspector $image:windows-$TRAVIS_TAG-2016
 do
   sleep 15
   echo "Try again"
 done
-echo "Waiting for Windows image stefanscherer/winspector:windows-$TRAVIS_TAG-1709"
-until docker run --rm winspector stefanscherer/winspector:windows-$TRAVIS_TAG-1709
+echo "Waiting for Windows image $image:windows-$TRAVIS_TAG-1709"
+until docker run --rm winspector $image:windows-$TRAVIS_TAG-1709
 do
   sleep 15
   echo "Try again"
@@ -28,8 +29,6 @@ chmod +x docker
 
 set -x
 
-image="stefanscherer/winspector
-"
 echo "Pushing manifest $image:$TRAVIS_TAG"
 ./docker manifest create "$image:$TRAVIS_TAG" \
   "$image:linux-$TRAVIS_TAG" \
