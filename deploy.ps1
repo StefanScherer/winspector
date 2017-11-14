6a706db82db6cbb86367e297956bc6bd7143f2ea
Stefan Scherer <scherer_stefan@icloud.com>
2017-11-14 21:51:17 +0100

    Rebase insider 17035 and add to manifest list

--- deploy.ps1
+++ deploy.ps1
@@ -14,4 +14,11 @@ docker tag winspector stefanscherer/winspector:windows-$version-2016
 docker push stefanscherer/winspector:windows-$version-2016
 
 npm install -g rebase-docker-image
-rebase-docker-image stefanscherer/winspector:windows-$version-2016 -t stefanscherer/winspector:windows-$version-1709 -b microsoft/nanoserver:1709
+rebase-docker-image stefanscherer/winspector:windows-$version-2016 `
+  -t stefanscherer/winspector:windows-$version-1709 `
+  -b microsoft/nanoserver:1709
+
+rebase-docker-image stefanscherer/winspector:windows-$version-2016 `
+  -s microsoft/nanoserver:10.0.14393.1770 `
+  -t stefanscherer/winspector:windows-$version-insider-17035 `
+  -b microsoft/nanoserver-insider:10.0.17035.1000
