aff1f95ace8686625553cc3caff3145e57340fab
Stefan Scherer <scherer_stefan@icloud.com>
2018-07-19 18:06:45 +0200

    Run more tests

--- test.ps1
+++ test.ps1
@@ -3,3 +3,4 @@ Write-Host Starting test
 $ErrorActionPreference = 'Stop';
 Write-Host Testing container
 docker run --rm winspector microsoft/windowsservercore
+docker run --rm winspector mcr.microsoft.com/windows-insider
